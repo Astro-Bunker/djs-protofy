8b18f6c46bf1c1afc9ea427d2d2d86c3f18e116b
Rodrig Couto <rodrigo.couto.santos@hotmail.com>
2024-02-17 15:15:33 -0300

    nice

--- src/core.ts
+++ src/core.ts
@@ -3,6 +3,10 @@ import { GuildChannels } from "./prototypes/guildChannels";
 import { GuildMembers } from "./prototypes/guildMembers";
 import { Guilds } from "./prototypes/guilds";
 import { Users } from "./prototypes/users";
+import { Emojis } from "./prototypes/emojis";
+import { GuildEmojis } from "./prototypes/guildEmojis";
+import { GuildMessages } from "./prototypes/guildMessages";
+import { Roles } from "./prototypes/roles";
 import { verifyDJSVersion } from "./utils";
 
 export class DJSEasier {
@@ -14,6 +18,10 @@ export class DJSEasier {
     new GuildMembers();
     new Guilds();
     new Users();
+    new Emojis();
+    new GuildEmojis();
+    new GuildMessages();
+    new Roles();
   }
 }
 
