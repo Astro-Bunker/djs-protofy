d1332eb6501769b091df7e2b124d49c319f95bfe
Gorniaky <hwss.s2@gmail.com>
2024-02-17 10:11:01 -0300

    .

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
 import { readFileSync } from "fs";
+import { join } from "path";
 
-export const suportedDJSVersion = Number(JSON.parse(readFileSync("../package.json", "utf8"))
+const packagePath = join(__dirname, "..", "package.json");
+
+export const suportedDJSVersion = Number(JSON.parse(readFileSync(packagePath, "utf8"))
   .devDependencies["discord.js"].replace(/(^\D*)|(\D+.*$)/g, ""));
