6863dc40b370b4368726914584da03182723e691
Gorniaky <hwss.s2@gmail.com>
2024-02-18 14:19:14 -0300

    .

--- src/prototypes/applicationCommand.ts
+++ src/prototypes/applicationCommand.ts
@@ -1,17 +1,31 @@
 import { ApplicationCommand } from "discord.js";
 
 export class AppCommand {
-  declare id: ApplicationCommand["id"];
-  declare name: ApplicationCommand["name"];
+  declare id: string;
+  declare name: string;
+  declare mention: string;
 
   constructor() {
     Object.defineProperties(ApplicationCommand.prototype, {
-      mention: { get() { return this.toString(); } },
+      mention: { get() { return `</${this.name}:${this.id}>`; } },
       toString: { value: this.toString },
     });
   }
 
+  getMention(): `</${string}:${string}>`;
+  getMention<G extends string>(subGroup: G): `</${string} ${G}:${string}>`;
+  getMention<G extends string, S extends string>(subGroup: G, subCommand: S): `</${string} ${G} ${S}:${string}>`;
+  getMention(subGroup?: string, subCommand?: string) {
+    if (subCommand)
+      return `</${this.name} ${subGroup} ${subCommand}:${this.id}>`;
+
+    if (subGroup)
+      return `</${this.name} ${subGroup}:${this.id}>`;
+
+    return this.mention;
+  }
+
   toString() {
-    return `</${this.name}:${this.id}>`;
+    return this.mention;
   }
 }
