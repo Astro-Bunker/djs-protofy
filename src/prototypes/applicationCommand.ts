0e41556a16a89a7145bafaa04dd950e5e9c4f5f3
Gorniaky <hwss.s2@gmail.com>
2024-02-18 21:43:37 -0300

    .

--- src/prototypes/applicationCommand.ts
+++ src/prototypes/applicationCommand.ts
@@ -2,7 +2,7 @@ import { ApplicationCommand } from "discord.js";
 
 export class AppCommand {
   declare id: string;
-  declare name: ApplicationCommand["name"];
+  declare name: string;
   declare mention: string;
 
   constructor() {
