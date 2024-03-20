bd1400011097d3918e2aafef4383a9b1b756d679
Gorniaky <hwss.s2@gmail.com>
2024-03-20 20:04:07 -0300

    fix

--- src/test/prototypes/string.test.ts
+++ src/test/prototypes/string.test.ts
@@ -7,7 +7,7 @@ new SString();
 
 const STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mollis dolor, quis egestas lectus. Pellentesque libero erat, faucibus ut scelerisque efficitur, pellentesque sit amet massa. Donec egestas ullamcorper mi eu ullamcorper. Maecenas pulvinar enim ultrices vehicula maximus. In varius justo vel tellus feugiat tincidunt. Etiam pulvinar sollicitudin odio vel porttitor. Nulla vehicula quis magna sed maximus. Curabitur rutrum, magna ut semper semper, lorem risus accumsan ex, nec iaculis nulla tellus a augue." as const;
 
-describe("testind the String#limit", () => {
+describe("Testing the String#limit", () => {
   // @ts-expect-error ts(2769)
   const keys = Object.keys(DiscordStringLimits).filter(isNaN) as (keyof typeof DiscordStringLimits)[];
 
