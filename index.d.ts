17b2e4b74a2475f199f74ec479d6b357e887786e
Gorniaky <hwss.s2@gmail.com>
2024-09-11 18:57:54 -0300

    feat(category channel children): create prototypes

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,7 @@ import type { AwaitOptions } from "./src/@types";
 import type { SApplicationCommand } from "./src/prototypes/applicationCommand";
 import type { ApplicationCommands } from "./src/prototypes/applicationCommands";
 import type { SArray } from "./src/prototypes/array";
+import type { CategoryChannelChildren } from "./src/prototypes/categoryChannelChildren";
 import type { Channels } from "./src/prototypes/channels";
 import type { SClient } from "./src/prototypes/client";
 import type { SCollection } from "./src/prototypes/collection";
@@ -42,6 +43,8 @@ declare module "discord.js" {
   > { }
 
   interface BaseGuildEmojiManager extends Emojis { }
+
+  interface CategoryChannelChildManager extends CategoryChannelChildren { }
 
   interface ChannelManager extends Channels { }
 
