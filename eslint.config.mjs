124bc491759fb0d4accb7555cb4965e757c19e64
Gorniaky <hwss.s2@gmail.com>
2024-10-30 15:43:13 -0300

    add eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
   {
     rules: {
       "@typescript-eslint/consistent-type-imports": ["warn", { fixStyle: "inline-type-imports" }],
+      "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": ["warn", {
           argsIgnorePattern: "^_",
