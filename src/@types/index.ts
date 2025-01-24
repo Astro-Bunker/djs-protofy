2ab5996b77a88f449ea6a5e70142598efa48d68f
Gorniaky <hwss.s2@gmail.com>
2025-01-24 14:37:02 -0300

    fix(collection): add toArray method adaptation

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,8 +1,10 @@
-import type { Channel, ChannelType, GuildBasedChannel, GuildChannelType } from "discord.js";
+import { type Channel, type ChannelType, type EnumLike, type GuildBasedChannel, type GuildChannelType } from "discord.js";
+
+export type EnumResolvable<E extends EnumLike<keyof E, unknown> = any, K extends keyof E = keyof E> = K | E[K];
 
 export type ChannelTypeString = keyof typeof ChannelType;
 
-export type GuildChannelTypeString = Exclude<keyof typeof ChannelType, "DM" | "GroupDM">;
+export type GuildChannelTypeString = Exclude<ChannelTypeString, "DM" | "GroupDM">;
 
 export type ChannelWithType<T extends ChannelType | ChannelTypeString> =
   Extract<Channel, { type: T extends string ? (typeof ChannelType)[T] : T }>;
