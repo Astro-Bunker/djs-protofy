f8937c0ee0dac32a08e8cd52a520b7b52b086299
Gorniaky <hwss.s2@gmail.com>
2025-02-27 10:10:55 -0300

    fix djs version verifier

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -4,6 +4,6 @@ import { join } from "path";
 let packageJSON;
 try { packageJSON = JSON.parse(readFileSync(join(__dirname, "..", "..", "package.json"), "utf8")); } catch { }
 
-export const suportedDJSVersion = packageJSON
+export const targetDJSVersion = packageJSON
   ? Number(packageJSON.devDependencies["discord.js"].replace(/(^\D+)|(\D+.*$)/g, ""))
-  : 0;
+  : NaN;
--- src/core/utils/djsVersionValidator.ts
+++ src/core/utils/djsVersionValidator.ts
@@ -1,10 +1,8 @@
 import { version } from "discord.js";
-import { suportedDJSVersion } from "./constants";
+import { targetDJSVersion } from "./constants";
 
 export function verifyDJSVersion() {
-  const v = Number(version.split(".")[0]);
-
-  if (v > suportedDJSVersion) {
-    process.emitWarning(`Expected discord.js@${suportedDJSVersion}. Some features may not work correctly.`);
+  if (!isNaN(targetDJSVersion) && targetDJSVersion !== Number(version.split(".")[0])) {
+    process.emitWarning(`Expected discord.js@${targetDJSVersion}. Some features may not work correctly.`);
   }
 }
