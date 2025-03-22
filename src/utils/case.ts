674aad9904ab735590cbe8a1c780e697efc62385
Gorniaky <hwss.s2@gmail.com>
2025-03-22 15:01:37 -0300

    improves snakify

--- src/utils/case.ts
+++ src/utils/case.ts
@@ -1,47 +1,49 @@
 import { exists } from ".";
 
-type SnakeCaseSeparator = "_";
-
-export type Snakify<T, Shallow extends boolean = false> =
+export type Snakify<T, Sep extends string = typeof defaultSnakeCaseSeparator, Shallow extends boolean = false> =
   T extends string
-  ? StringSnakify<T>
+  ? StringSnakify<T, Sep>
   : T extends object
-  ? ObjectSnakify<T, Shallow>
+  ? ObjectSnakify<T, Sep, Shallow>
   : T;
 
-type StringSnakify<S extends string> = FixStringSnakeCase<StringSnakifyFirstStep<S>>;
+type StringSnakify<S extends string, Sep extends string> = FixStringSnakeCase<StringSnakifyFirstStep<S, Sep>, Sep>;
 
-type StringSnakifyFirstStep<S extends string> =
+type StringSnakifyFirstStep<S extends string, Sep extends string> =
   S extends `${infer A}${infer B}`
-  ? `${A extends SnakeCaseSeparator ? "" : A extends Capitalize<A> ? SnakeCaseSeparator : ""}${Lowercase<A>}${StringSnakifyFirstStep<B>}`
+  ? `${A extends Sep ? "" : A extends Capitalize<A> ? Sep : ""}${Lowercase<A>}${StringSnakifyFirstStep<B, Sep>}`
   : S;
 
-type FixStringSnakeCase<S extends string> =
+type FixStringSnakeCase<S extends string, Sep extends string> =
   S extends `${infer A}${infer B}`
-  ? `${A extends SnakeCaseSeparator ? "" : A}${B}`
+  ? `${A extends Sep ? "" : A}${B}`
   : S;
 
-type ObjectSnakify<O, Shallow extends boolean = false> =
+type ObjectSnakify<O, Sep extends string, Shallow extends boolean = false> =
   O extends Array<infer U>
-  ? Array<ObjectSnakify<U, Shallow>>
+  ? Array<ObjectSnakify<U, Sep, Shallow>>
   : O extends object
-  ? { [K in keyof O as StringSnakify<string & K>]: Shallow extends true ? O[K] : ObjectSnakify<O[K], Shallow> }
+  ? { [K in keyof O as StringSnakify<string & K, Sep>]: Shallow extends true ? O[K] : ObjectSnakify<O[K], Sep, Shallow> }
   : O;
 
-export function snakify<U>(U: U): Snakify<U>;
-export function snakify<U, Shallow extends boolean>(U: U, Shallow: Shallow): Snakify<U, Shallow>;
-export function snakify<S extends string>(S: S): Snakify<S>;
-export function snakify<O extends object>(O: O): Snakify<O>;
-export function snakify<O extends object, Shallow extends boolean>(O: O, Shallow: Shallow): Snakify<O, Shallow>;
-export function snakify(u: unknown, s = false) {
+const defaultSnakeCaseSeparator = "_" as const;
+
+export function snakify<U, Sep extends string>(U: U, Sep?: Sep): Snakify<U, Sep>;
+export function snakify<U, Sep extends string, Shallow extends boolean>(U: U, Shallow: Shallow, Sep?: Sep): Snakify<U, Sep, Shallow>;
+export function snakify<S extends string, Sep extends string>(S: S, Sep?: Sep): Snakify<S, Sep>;
+export function snakify<O extends object, Sep extends string>(O: O, Sep?: Sep): Snakify<O, Sep>;
+export function snakify<O extends object, Sep extends string, Shallow extends boolean>(O: O, Shallow: Shallow, Sep?: Sep): Snakify<O, Sep, Shallow>;
+export function snakify(u: unknown, shallow: any = false, sep: any = defaultSnakeCaseSeparator) {
   if (!exists(u)) return u;
+
+  if (typeof shallow === "string") return snakify(u, sep, shallow);
 
   if (typeof u === "string")
     return u.replace(/(^[A-Z])|([A-Z])/g, (_, a, b) => a ? a.toLowerCase() : `_${b.toLowerCase()}`);
 
   if (Array.isArray(u))
-    return u.map(v => typeof v === "object" ? snakify(v, s) : v);
+    return u.map(v => typeof v === "object" ? snakify(v, shallow, sep) : v);
 
   return Object.entries(u)
-    .reduce((a, [k, v]) => Object.assign(a, { [snakify(k)]: s ? v : typeof v === "object" ? snakify(v, s) : v }), {});
+    .reduce((a, [k, v]) => Object.assign(a, { [snakify(k)]: shallow ? v : typeof v === "object" ? snakify(v, shallow) : v }), {});
 }
